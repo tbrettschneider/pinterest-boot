e128e7d8505639ff8cbce85c2ca62d96c83ebaeb
tommy <tommy.brettschneider@gmail.com>
2017-11-03 19:50:02 +0100

    initial import

---
+++ src/main/java/de/tb/showroom/pinterest/PinterestApplication.java
@@ -0,0 +1,12 @@
+package de.tb.showroom.pinterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinterestApplication.class, args);
+	}
+}
